fd9332267cf925a1150fd9eafe4b804772077031
Jinglu1982 <34806503+Jinglu1982@users.noreply.github.com>
2021-10-21 10:17:11 +0800

    Update variant_pipeline.py

--- variant_pipeline.py
+++ variant_pipeline.py
@@ -1,13 +1,7 @@
 #use global wildcard make sample list#
 SAMPLE_LIST, = glob_wildcards("0.rawdata/{sample}_L001_R1_001.fastq.gz")
 ref =""data/indexRef.fa"
-
-rule all:
-    input:
-        expand("3.mapping/{sample}.bam",sample=SAMPLE_LIST),
-        expand("4.snp/{sample}.snp.tsv",sample=SAMPLE_LIST),
-        expand("5.mpileup/{sample}.mpileup.tsv",sample=SAMPLE_LIST),
-        expand("6.consensus/{sample}.fa",sample=SAMPLE_LIST)
+primer="data/v3primer_indexref.bed"
 
 rule qc:
     input:
@@ -28,36 +22,25 @@ rule cutadapt:
         seq2=rules.qc.output.qc2
     output:
         bamfile="2.cutadapt/{sample}.sorted.bam",
-        cutseq1="2.cutadapt/{sample}.cutadapt.1.fq",
-        cutseq2="2.cutadapt/{sample}.cutadapt.2.fq",
+        trimbam="2.cutadapt/{sample}.trim.bam"
     params:
         prefix="2.cutadapt/{sample}.trimmed"
     shell:
         """
-		bwa mem -t 20 -a {ref} {input.seq1} {input.seq2} | samtools view -@ 4 -bS -F 4 | samtools sort -@ 4 -o {output.bamfile};
-		ivar trim -b {primer} -p {params.prefix} -i {output.bamfile} -q 15 -s 4;
-        bedtools bamtofastq -i {params.prefix}.bam -fq {output.cutseq1} -fq2 {output.cutseq2};
-        rm {params.prefix}.bam
+		bwa mem -t 20 -a {ref2} {input.seq1} {input.seq2} | samtools view -@ 4 -bS -F 4 | samtools sort -@ 4 -o {output.bamfile};
+		ivar trim -b {primer} -p {params.prefix} -i {output.bamfile} -q 15 -s 4 -e -m 10;
+		samtools sort {params.prefix}.bam -o {output.trimbam};
+		rm {params.prefix}.bam {output.bamfile};
         """
-rule mapping:
-    input:
-        seq1=rules.cutadapt.output.cutseq1,
-        seq2=rules.cutadapt.output.cutseq2
-    output:
-        bamfile="3.mapping/{sample}.bam"
-
-    shell:
-        "bwa mem -t 20 -a {ref} {input.seq1} {input.seq2}| samtools view -@ 4 -bS -F 4 | samtools sort -@ 4 -o {output.bamfile};"
-        "samtools index {output.bamfile}"
 rule mpileup:
     input:
-        bamfile=rules.mapping.output.bamfile
+        bamfile=rules.cutadapt.output.trimbam
     output:
         snptab="4.snp/{sample}.snp.tsv",
         mpileuptab="5.mpileup/{sample}.mpileup.tsv",
         consensusseq="6.consensus/{sample}.fa"
 
     shell:
-        "samtools mpileup -Q 20 -d 100000 --reference {ref} {input.bamfile} | ivar variants -r {ref} -p {output.snptab} -t 0;"
-        "bcftools mpileup --threads 2 -Q 20 -a FORMAT/DP,FORMAT/AD,INFO/AD -I -d 100000 -f {ref} {input.bamfile} > {output.mpileuptab};"
+        "samtools mpileup -Q 20 -d 100000 --reference {ref2} {input.bamfile} | ivar variants -r {ref2} -p {output.snptab} -t 0;"
+        "bcftools mpileup --threads 2 -Q 20 -a FORMAT/DP,FORMAT/AD,INFO/AD -I -d 100000 -f {ref2} {input.bamfile} > {output.mpileuptab};"
         "samtools mpileup {input.bamfile} | ivar consensus -p {output.consensusseq} -q 20 -t 0 -n N -m 10"
